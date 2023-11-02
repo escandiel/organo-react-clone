b4e66a93f9ee38e5fb6ebda8f5e10e48f9c5abcd
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-02 17:39:43 -0300

    creating delete icon

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,21 @@
-import './colaborador.css'
+import "./colaborador.css";
+import { AiFillDelete } from "react-icons/ai";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
-}
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+  return (
+    <div className="colaborador">
+      <AiFillDelete size={30} className="deletar" onClick={aoDeletar}>
+        deletar
+      </AiFillDelete>
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+      </div>
+    </div>
+  );
+};
 
-export default Colaborador
+export default Colaborador;
